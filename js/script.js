a3fec08d687dadac16e1480a490924bcce248030
Komaritskaya <komaritskaya@gmail.com>
2019-02-11 10:15:29 +0300

    upd

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+$(document).ready(function(){
+    // весь ваш код c jQuery
+  
+    // SLIDER
+  
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+      });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+        });
+}
 // function ready(fn) {
 //   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
 //     fn();
